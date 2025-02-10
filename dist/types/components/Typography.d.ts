05978e107d7c60b0b5a4adcf75187aba94b3a58f
superredstar <alexander116gm@gmail.com>
2025-02-10 15:35:24 -0500

    Update
    Add version
    Show dist only,

---
+++ dist/types/components/Typography.d.ts
@@ -0,0 +1,3 @@
+import { TypographyProps } from './Typography.interface';
+/** Primary UI component for user interaction */
+export declare const Typography: ({ children, ...props }: TypographyProps) => import("react/jsx-runtime").JSX.Element;
